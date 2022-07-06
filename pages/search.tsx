cfda72e38c4f2e33bbb25e2d69a6292f6abb1e54
Ricardo Alba <ralba@radarappsecurity.com>
2022-07-05 20:44:50 -0500

    day2

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import {format} from "date-fns";
-import InfoCard from "../components/infoCard";
+import InfoCard from "../components/InfoCard";
 
 function Search({ searchResults }:any) {
     
